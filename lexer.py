449cb3c3608230ec2db97974ce9520f1304c1e9e
alejandro.castrillon <alejandro.castrillon@utp.edu.co>
2019-08-30 11:53:04 -0500

    Deteccion de error cuando un comentario multilinea no se ha cerrado

--- lexer.py
+++ lexer.py
@@ -3,22 +3,57 @@ import sly
 
 class Lexer(sly.Lexer):
 
-    keywords = {'if', 'else', 'for', 'do', 'while', 'return', 'break', 'not', 'int', 'char', 'delete', 'void', 'float', 'size', 'bool', 'true', 'false',
-    'const'}
+    keywords = {'if', 'else', 'forx', 'do', 'while', 'return', 'break', 'not', 'int', 'char', 'delete', 'void', 'float', 
+    'size', 'bool', 'true', 'false', 'const'}
 
     tokens = {
         * { kw.upper() for kw in keywords},
-        LE, GE, EQ, NE, LT, GT, OR, AND, NOT, IDENT, INT_LIT, FLOAT_LIT, BOOL_LIT, CHAR_LIT, STRING_LIT, INC, DEC, ADDEQ, SUBEQ, MULEQ, DIVEQ, MULTILINE_COMMENT, MODEQ
-    }
+        LE, GE, EQ, NE, LT, GT, OR, AND, NOT, INT_LIT, FLOAT_LIT, IDENT, BOOL_LIT, CHAR_LIT, STRING_LIT, INC, DEC, 
+        ADDEQ, SUBEQ, MULEQ, DIVEQ, MODEQ}
     
     literals = {'(', ')', '{', '}', ';', ',', '.', '+', '-', '*', '/', '%', '<', '>', '=', '!', '[', ']', ' '}
 
-    ignore = '\t'
+    ignore = '\t\n'
 
-    ignore_multiline_comment = r'\/\*[.]*\*\/'
-    ignore_line_comment = r'\/\/.*'
+    LE = r'<='
 
-    FLOAT_LIT = r'[-]?[0-9]+[.][0-9]+'
+    GE = r'>='
+
+    EQ = r'=='
+
+    LT = r'<'
+
+    GT = r'>'
+
+    OR = r'\|\|'
+
+    AND = r'\&\&'
+
+    INC = r'\+\+'
+
+    DEC = r'\-\-'
+
+    ADDEQ = r'\+\='
+
+    SUBEQ = r'\-\='
+
+    MULEQ = r'\*\='
+
+    DIVEQ = r'\/\='
+
+    MODEQ = r'\%\='
+
+    ignore_multiline_comment = r'\/\*.*\*\/'
+    ignore_line_comment = r'\/\/.*\n'
+
+    @_(r'\/\*.*')
+    def multilineCommentNotClosedError(self, t):
+        error(self.lineno, 'Comentario no cerrado')
+        self.index += 1
+
+
+    FLOAT_LIT = r'[-]?[0-9]+[.][0-9]*'
+    
 
     @_(r'0[bB][01]+', r'[1-9][0-9]*', r'0[xX][0-9a-fA-F]+')
     def INT_LIT(self, t):
@@ -45,7 +80,7 @@ class Lexer(sly.Lexer):
     
 def main():
     import sys
-    text = 'hola=-9656.75;mychar="a";mystring="hello world"//Te amo/*You are the best'
+    text = 'hola=-9656.75;\nmychar="a";\nmystring="hello world";\n//Te amo\n/*You are the best*/'
     lexer = Lexer()
     for tok in lexer.tokenize(text):
         print(tok)
